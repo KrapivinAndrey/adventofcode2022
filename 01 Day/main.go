7392ea546a4bc5ee2b641b99ec47737e7681475e
a.krapivin <a.krapivin@kontur.ru>
2022-12-01 19:29:28 +0500

    day 01

---
+++ 01 Day/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+)
+
+func main() {
+	var (
+		sum = 0
+	)
+	f, err := os.Open("input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	elves := []int{1}
+	for scanner.Scan() {
+		s := scanner.Text()
+		if s == "" {
+			elves = append(elves, sum)
+			sum = 0
+		} else {
+			intValue := 0
+			fmt.Sscan(s, &intValue)
+			sum = sum + intValue
+		}
+	}
+	elves = append(elves, sum)
+	sort.Ints(elves)
+
+	fmt.Println(elves[len(elves)-1] + elves[len(elves)-2] + elves[len(elves)-3])
+
+}
